8659f30351654d4a354822957a483b28c5b227a5
Jim Blackler <jimblackler@google.com>
2022-05-25 00:50:23 +0100

    Generator version of brute force scan.

--- lib/lsScanBrute.ts
+++ lib/lsScanBrute.ts
@@ -1,23 +1,19 @@
-function scan(length: number, canFollow: (i0: number, i1: number) => boolean,
-              start: number, base: number) {
+export function* lsScanBrute(length: number, canFollow: (i0: number, i1: number) => boolean,
+                             start = 0, base = -1) {
   if (start === length) {
-    return [];
+    return;
   }
 
-  const skipSequence: number[] = scan(length, canFollow, start + 1, base);
+  const skipSequence: number[] = [...lsScanBrute(length, canFollow, start + 1, base)];
 
   if (base === -1 || canFollow(base, start)) {
-    const thisSequence: number[] = [start].concat(scan(length, canFollow, start + 1, start));
-    if (thisSequence.length >= skipSequence.length) {
-      return thisSequence;
+    const thisSequence: number[] = [...lsScanBrute(length, canFollow, start + 1, start)];
+
+    if (thisSequence.length + 1 >= skipSequence.length) {
+      yield start;
+      yield* thisSequence;
+      return;
     }
   }
-  return skipSequence;
-}
-
-export function* lsScanBrute(length: number, canFollow: (i0: number, i1: number) => boolean) {
-  const out = scan(length, canFollow, 0, -1);
-  for (const v of out) {
-    yield v;
-  }
+  yield* skipSequence;
 }
